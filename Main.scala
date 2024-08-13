84021c53ad7217a96ee3463513814c6466d3e578
horothesun <horothesun@gmail.com>
2024-08-13 10:59:13 +0100

    Splitting munit and munit-scalacheck versioning

--- Main.scala
+++ Main.scala
@@ -11,7 +11,7 @@
 //> using test.dep org.typelevel::cats-laws:2.12.0
 //> using test.dep org.scalameta::munit:1.0.1
 //> using test.dep org.typelevel::munit-cats-effect:2.0.0
-//> using test.dep org.scalameta::munit-scalacheck:1.0.1
+//> using test.dep org.scalameta::munit-scalacheck:1.0.0
 //> using test.dep org.typelevel::cats-effect-testkit:3.5.4
 //> using test.dep org.typelevel::discipline-munit:2.0.0
 
